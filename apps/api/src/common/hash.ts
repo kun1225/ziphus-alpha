28ecdee9b8aa9342c2817878ddd5a83e65e7cd96
Ray <62081739+SR0725@users.noreply.github.com>
2024-03-29 03:04:18 +0800

    fix: crypto is not defined

--- apps/api/src/common/hash.ts
+++ apps/api/src/common/hash.ts
@@ -1,6 +1,8 @@
+import { subtle } from "node:crypto";
+
 async function hash(data: string) {
   const msgUint8 = new TextEncoder().encode(data);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
+  const hashBuffer = await subtle.digest("SHA-256", msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray
     .map((b) => b.toString(16).padStart(2, "0"))
