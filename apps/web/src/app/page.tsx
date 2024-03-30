ff415fadc17fc1ee0a5dc1bc2e01939629d4a134
SR0725 <ray948787@gmail.com>
2024-03-31 01:02:37 +0800

    feat: sidebar 擴增實用性

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Page(): JSX.Element {
   return (
     <div className="min-w-screen h-full min-h-screen w-full bg-[#0E0E0E]">
       <Header />
-      <main className=" container mx-auto py-4">
+      <main className=" container mx-auto py-4 flex flex-col">
+        <Link href="/spaces" className="text-white">Space List</Link>
         <Link href="/cards" className="text-white">Card List</Link>
       </main>
     </div>
--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -1,36 +1,97 @@
 'use client';
-import { MdTipsAndUpdates } from 'react-icons/md';
+import { MdTipsAndUpdates, MdHomeFilled } from 'react-icons/md';
 import { Button } from './material-tailwind';
 import SidebarContainer from './sidebar-container';
 import { useState } from 'react';
+import { TbLayoutSidebarLeftExpand, TbLayoutSidebarLeftCollapse } from 'react-icons/tb';
+import { useRouter } from 'next/navigation';
+import { FaMap } from 'react-icons/fa';
+import { PiCardsBold } from 'react-icons/pi';
 
 function Sidebar() {
   const [display, setDisplay] = useState<'static' | 'float'>('static');
+  const router = useRouter();
   return (
-    <SidebarContainer display={display}>
-      <div className="flex justify-between">
-        <Button variant="text" className="flex flex-1 justify-start" size="sm">
+    <>
+      {
+        display === 'float' && (
+          <div className=' fixed top-10 left-2 z-50'>
+            <Button variant="text" className="flex w-12 h-12 justify-start" size="sm" onClick={() => setDisplay('static')}>
+              <span className="text-white">
+                <TbLayoutSidebarLeftExpand />
+              </span>
+            </Button>
+          </div>
+        )
+      }
+
+      <SidebarContainer display={display}>
+        <div className="flex justify-between">
+          <Button variant="text" className="flex flex-1 justify-start" size="sm">
+            <h1 className="text-md font-bold text-gray-400">
+              <span className="mr-2  text-lg text-white">
+                <MdTipsAndUpdates className="inline-block" />
+              </span>
+              Ziphus
+            </h1>
+          </Button>
+          <Button
+            variant="text"
+            className="flex justify-center items-center w-12 h-12"
+            size="sm"
+            onClick={() => setDisplay(display === 'static' ? 'float' : 'static')}
+          >
+            {display === 'static' ? (
+              <span className="text-white">
+                <TbLayoutSidebarLeftCollapse />
+              </span>
+            ) : (
+              <span className="text-white">
+                <TbLayoutSidebarLeftExpand />
+              </span>
+            )}
+          </Button>
+        </div>
+        <Button variant="text" className="flex flex-1 justify-start" size="sm"
+          onClick={() => {
+            router.push('/');
+          }}
+        >
           <h1 className="text-md font-bold text-gray-400">
             <span className="mr-2  text-lg text-white">
-              <MdTipsAndUpdates className="inline-block" />
+              <MdHomeFilled className="inline-block" />
             </span>
-            Ziphus
+            Home
           </h1>
         </Button>
-        <Button
-          variant="text"
-          className="flex w-8 justify-start"
-          size="sm"
-          onClick={() => setDisplay(display === 'static' ? 'float' : 'static')}
+        <Button variant="text" className="flex flex-1 justify-start" size="sm"
+          onAbort={() => {
+            router.push('/spaces');
+          }}
         >
-          {display === 'static' ? (
-            <span className="text-white">🔒</span>
-          ) : (
-            <span className="text-white">🔓</span>
-          )}
+          <h1 className="text-md font-bold text-gray-400">
+            <span className="mr-2  text-lg text-white">
+              <FaMap className="inline-block" />
+            </span>
+            Spaces
+          </h1>
         </Button>
-      </div>
-    </SidebarContainer>
+        <Button variant="text" className="flex flex-1 justify-start" size="sm"
+          onAbort={() => {
+            router.push('/cards');
+          }}
+        >
+          <h1 className="text-md font-bold text-gray-400">
+            <span className="mr-2  text-lg text-white">
+              <PiCardsBold className="inline-block" />
+            </span>
+            Cards
+          </h1>
+        </Button>
+
+
+      </SidebarContainer>
+    </>
   );
 }
 
