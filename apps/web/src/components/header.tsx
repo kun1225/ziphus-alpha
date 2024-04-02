d1fb901681d3490990ef0c0b09233dff84d55540
Ray <62081739+SR0725@users.noreply.github.com>
2024-04-02 18:17:30 +0800

    feat: header add space button

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,15 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 function Header() {
   return (
-    <header className="h-fit w-full bg-[#00000033] backdrop-blur-sm border-b border-b-[#ffffff44]">
-      <div className="container mx-auto flex w-full items-center justify-start xl:py-4">
-        <Link
-          href="/"
-          className="flex cursor-pointer select-none items-center gap-2"
-        >
-          <h1 className=" text-2xl font-bold text-white">Ziphus</h1>
-        </Link>
+    <header className="h-fit w-full border-b border-b-[#ffffff44] bg-[#00000033] backdrop-blur-sm">
+      <div className="container mx-auto flex w-full items-center justify-between xl:py-4">
+        <div>
+          <Link
+            href="/"
+            className="flex cursor-pointer select-none items-center gap-2"
+          >
+            <h1 className=" text-2xl font-bold text-white">Ziphus</h1>
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="/spaces"
+            className="flex cursor-pointer select-none items-center gap-2"
+          >
+            <h1 className="text-white">My Spaces</h1>
+          </Link>
+        </div>
       </div>
     </header>
   );
